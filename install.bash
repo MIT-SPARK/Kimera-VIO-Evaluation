d510004f6f7adb389b1d2e81a23feb784c1f9718
Toni Rosinol <tonirosinol@hotmail.com>
2018-12-03 01:12:11 -0500

    Update

--- install.bash
+++ install.bash
@@ -1,4 +1,5 @@
-pip install evo --upgrade --no-binary evo
+git clone git@github.com:ToniRV/evo-1.git evo
+pip install ./evo --upgrade --no-binary evo
 pip install pyyaml
 
 # y virtualenv necesita correr en python2, python3 la lia amb tkinter
