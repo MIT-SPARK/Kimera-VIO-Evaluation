1d30be767ce607f269f96cba58aa57e85dfe1f15
Toni Rosinol <tonirosinol@hotmail.com>
2018-12-07 17:49:19 -0500

    Add setup.py and rename src

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='spark_vio_evaluation',
+      version='0.1',
+      description='Code for evaluating the performance of the SPARK VIO pipeline',
+      url='https://github.com/ToniRV/spark_vio_evaluation',
+      author='Antoni Rosinol',
+      author_email='arosinol@mit.edu',
+      license='MIT',
+      packages=['evaluation'],
+      install_requires=['numpy', 'pyyaml', 'evo-1'],
+      zip_safe=False)
