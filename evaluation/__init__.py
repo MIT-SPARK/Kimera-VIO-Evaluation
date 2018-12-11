e11f6680ea3e53b0f04de9022df2f3b9c8a8680f
Toni <tonirosinol@hotmail.com>
2018-12-10 23:59:16 -0500

    Move plotting to tools subpackage

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,2 +1,1 @@
-#!/usr/bin/env python3
-from .main_evaluation import run, parser
+from main_evaluation import run, parser
--- evaluation/tests/test_evaluation.py
+++ evaluation/tests/test_evaluation.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from unittest import TestCase
 
 import evaluation as ev
