50d8f3c1a6c88f1636b07ea4292d7d6b580b858c
Toni <tonirosinol@hotmail.com>
2019-07-19 16:58:07 -0400

    Allow for multiple tabs in html

---
+++ html/js/custom.js
@@ -0,0 +1,13 @@
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
