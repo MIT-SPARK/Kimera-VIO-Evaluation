096a10715f1f8aa745ee577d780af52099483f69
Toni <tonirosinol@hotmail.com>
2019-07-27 21:11:50 -0400

    Use glog in filesystem_utils

--- evaluation/tools/filesystem_utils.py
+++ evaluation/tools/filesystem_utils.py
@@ -2,37 +2,36 @@
 
 import os
 from shutil import rmtree, move
+import glog as log
 import errno
 
 def create_full_path_if_not_exists(filename):
     if not os.path.exists(os.path.dirname(filename)):
         try:
-            print('Creating non-existent path: %s' % filename)
+            log.info('Creating non-existent path: %s' % filename)
             os.makedirs(os.path.dirname(filename))
         except OSError as exc: # Guard against race condition
             if exc.errno != errno.EEXIST:
-                print("Could not create inexistent filename: " + filename)
-                raise
+                log.fatal("Could not create inexistent filename: " + filename)
 
 def move_output_from_to(from_dir, to_dir):
     try:
         if (os.path.exists(to_dir)):
             rmtree(to_dir)
     except:
-        print("Directory:" + to_dir + " does not exist, we can safely move output.")
+        log.info("Directory:" + to_dir + " does not exist, we can safely move output.")
     try:
         if (os.path.isdir(from_dir)):
             move(from_dir, to_dir)
         else:
-            print("There is no output directory...")
+            log.info("There is no output directory...")
     except:
         print("Could not move output from: " + from_dir + " to: " + to_dir)
         raise
     try:
         os.makedirs(from_dir)
     except:
-        print("Could not mkdir: " + from_dir)
-        raise
+        log.fatal("Could not mkdir: " + from_dir)
 
 def ensure_dir(dir_path):
     """ Check if the path directory exists: if it does, returns true,
