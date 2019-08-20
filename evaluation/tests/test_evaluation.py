7650d4d77a7b0d6970a3be170f31c955d4c20c01
Toni <tonirosinol@hotmail.com>
2019-08-20 16:33:10 -0400

    Fix tests

--- evaluation/tests/test_evaluation.py
+++ evaluation/tests/test_evaluation.py
@@ -13,21 +13,31 @@ class TestMainEvaluation(TestCase):
                                   '--save_results', '--save_plots', '--save_boxplots'])
         ev.run(args)
 
+        test_output_dir = 'evaluation/tests/test_results/V1_01_easy/S/'
         # Check that we have generated a results file.
-        results_file = 'evaluation/tests/test_results/V1_01_easy/S/results.yaml'
+        results_file = os.path.join(test_output_dir, 'results.yaml')
         self.assertTrue(os.path.isfile(results_file))
         # Remove file so that we do not re-test and get a false negative...
         os.remove(results_file)
 
         # Check that we have generated boxplots.
-        boxplots_file  = 'evaluation/tests/test_results/V1_01_easy/traj_relative_errors_boxplots.eps'
+        boxplots_file = os.path.join(test_output_dir, '../traj_relative_errors_boxplots.eps')
         self.assertTrue(os.path.isfile(boxplots_file))
         # Remove file so that we do not re-test and get a false negative...
         os.remove(boxplots_file)
 
         # Check that we have generated plots.
-        plots_file = 'evaluation/tests/test_results/V1_01_easy/S/plots.pdf'
-        self.assertTrue(os.path.isfile(plots_file))
-        # Remove file so that we do not re-test and get a false negative...
-        os.remove(plots_file)
-
+        plots_list = ['plots_APE_translation.eps',
+                      'plots_APE_translation_trajectory_error.eps',
+                      'plots_RPE_rotation.eps',
+                      'plots_RPE_rotation_trajectory_error.eps',
+                      'plots_RPE_translation.eps',
+                      'plots_RPE_translation_trajectory_error.eps']
+        for plot_filename in plots_list:
+            plot_filepath = os.path.join(test_output_dir, plot_filename)
+            self.assertTrue(os.path.isfile(plot_filepath))
+            try:
+                # Remove file so that we do not re-test and get a false negative...
+                os.remove(plot_filepath)
+            except:
+                raise Exception("Error while deleting file : ", plots_filepath)
