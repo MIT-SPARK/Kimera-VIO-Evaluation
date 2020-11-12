1cbd8679ad1ee55bc4e89deb40004f27d2342ed5
Toni <tonirosinol@hotmail.com>
2020-11-12 16:40:59 -0500

    Remove old spark_vio name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 import fastentrypoints
 
-setup(name='spark_vio_evaluation',
+setup(name='kimera_vio_evaluation',
       version='0.2',
-      description='Code for evaluating the performance of the SPARK VIO pipeline',
-      url='https://github.com/ToniRV/spark_vio_evaluation',
+      description='Code for evaluating the performance of the Kimera VIO pipeline',
+      url='https://github.com/ToniRV/Kimera-VIO-Evaluation',
       author='Antoni Rosinol',
       author_email='arosinol@mit.edu',
       license='MIT',
